51531b7802800689a82623a4f12444c93695e4e0
ADITYA KUMAR <152189776+ADITYA9759@users.noreply.github.com>
2025-02-07 09:56:31 +0530

    Update from flask import Flask, request, jsonif.py

--- from flask import Flask, request, jsonif.py
+++ from flask import Flask, request, jsonif.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import pandas as pd
 from flask_cors import CORS
 
-app = Flask(_name_)
+app = Flask(__name__)
 CORS(app)  # React/JS फ्रंटएंड से API एक्सेस के लिए
 
 @app.route('/upload', methods=['POST'])
@@ -23,5 +23,5 @@ def upload_file():
     
     return jsonify(summary)
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
